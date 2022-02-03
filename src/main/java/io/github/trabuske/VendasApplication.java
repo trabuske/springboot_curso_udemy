5ca7a94ee117425b47fb0220c436e26c8a8b6574
Tiago Rabuske <t.rabuske@gmail.com>
2022-02-03 15:21:11 -0300

    commit do hello world

--- src/main/java/io/github/trabuske/VendasApplication.java
+++ src/main/java/io/github/trabuske/VendasApplication.java
@@ -2,9 +2,17 @@ package io.github.trabuske;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello world 2!";		
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
