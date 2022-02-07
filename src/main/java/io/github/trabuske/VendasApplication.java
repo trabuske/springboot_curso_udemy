7447568b892517f5fe6e7dba56d769d0d40631a7
Tiago Rabuske <t.rabuske@gmail.com>
2022-02-07 17:20:30 -0300

    commit das aulas de configuracao do Springboot

--- src/main/java/io/github/trabuske/VendasApplication.java
+++ src/main/java/io/github/trabuske/VendasApplication.java
@@ -1,17 +1,24 @@
 package io.github.trabuske;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@ComponentScan
 @RestController
 public class VendasApplication {
 	
+	@Value("${application.name}")
+	private String applicationName;
+	
+	
 	@GetMapping("/hello")
 	public String helloWorld() {
-		return "Hello world 2!";		
+		return applicationName;		
 	}
 	
 	public static void main(String[] args) {
